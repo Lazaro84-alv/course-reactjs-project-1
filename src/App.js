407d959dbc95043923057728d4f4db5a1f492058
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-07-26 15:10:49 -0300

    Add PostCard components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Component } from 'react';
+import { PostCard } from './components/PostCard';
 
 class App extends Component {
     state = {
@@ -11,19 +12,6 @@ class App extends Component {
     }
 
     loadPosts = async () => {
-      const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-      
-      const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos'); 
-
-      const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-      const postsJson = await posts.json();
-      const photosJson = await photos.json();
-
-      const postsAndPhotos = postsJson.map((post, index) => {
-        return { ...post, cover: photosJson[index].url }
-      });
-
       this.setState({ posts: postsAndPhotos });
     } 
   
@@ -34,14 +22,13 @@ class App extends Component {
       <section className="container">
         <div className="posts">
         {posts.map(post => ( 
-          <div className="post">
-            <img src={post.cover} alt={post.title} />
-            <div key={post.id} 
-              className="post-content">
-              <h1 >{post.title}</h1>
-              <p>{post.body}</p>
-            </div>
-          </div>
+          <PostCard 
+            key={post.id}
+            title={post.title}
+            body={post.body}
+            id={post.id}
+            cover={post.cover}
+          />
           ))}
         </div>
       </section>
