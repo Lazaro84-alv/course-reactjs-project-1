b717e2a308b7c90f469ea3474a5018a04faa66e6
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-07-26 16:46:15 -0300

    Add styles in components

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,16 @@
 import { Component } from "react";
+import "./styles.css";
 
 export class Button extends Component {
   render() {
     const { text, onClick } = this.props;
 
     return (
-      <button onClick={onClick}> 
+      <button 
+        disabled 
+        className="button" 
+        onClick={onClick}
+      > 
         {text}
       </button>
     );
